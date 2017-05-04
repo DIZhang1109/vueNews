f01fab13c9024a48187a3a5979a70dd7ac098ed3
Di <taylorzhang0215@gmail.com>
2017-05-05 10:33:21 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 /**
  * Created by Di on 3/05/17.
  */
-const vm = new Vue({
-    el: '#vue-news',
+
+/*global Vue*/
+/*global axios*/
+
+new Vue({
+    el: 'app',
+    template: `
+        <div id="view">
+    		<h3 align="center">{{ title }}</h3>
+    		<div align="center">
+    			<select v-model="selected">
+    				<option value="">Home</option>
+    				<option v-for="section in newSections">{{ section }}</option>
+    			</select>
+    			<button @click="retrieveNews(selected)">Retrieve</button><br>
+    		</div>
+    		<h4>Top Stories of {{ selected }} Today: {{ news.length }}</h4>
+    		<ul>
+    			<li v-for="item in news">
+    				{{ item.title }}<br>
+    				<div v-if="item.multimedia.length >= 1">
+    					<img :src="item.multimedia[1].url"><br>
+    				</div>
+    				{{ item.abstract }}
+    			</li>
+    		</ul>
+		</div>
+    `,
     data: {
-        title: 'Vue News',
+        title: 'Vue Top News',
         news: [],
-        newsAmount: '',
-        selected: '',
         sections: [
             'opinion',
             'world',
@@ -35,25 +59,26 @@ const vm = new Vue({
             'obituaries',
             'insider'
         ],
-        formalSection: []
+        selected: ''
     },
     methods: {
-        retrieveNews: function (selected) {
-            if (selected === '') {
-                selected = 'Home';
+        retrieveNews: function(section) {
+            if (section == '') {
+                section = 'Home';
             }
-            axios.get('http://api.nytimes.com/svc/topstories/v2/' + selected.toLowerCase() + '.json?api-key=23bd2416129e4997bc0009d118e1e635')
-                .then(response => this.news = response.data.results)
-                .catch(error => console.log(error))
+            axios.get('//api.nytimes.com/svc/topstories/v2/' + section.toLowerCase() + '.json?api-key=23bd2416129e4997bc0009d118e1e635')
+                .then(response => {
+                    this.news = response.data.results;
+                })
+                .catch(error => console.log(error));
         }
     },
-    created() {
-        this.sections = this.sections.sort();
-        this.formalSection = this.sections.map(section => section.charAt(0).toUpperCase() + section.slice(1).toLowerCase());
+    computed: {
+        newSections: function() {
+            return this.sections.sort().map(section => section.charAt(0).toUpperCase() + section.slice(1).toLowerCase());
+        }
     },
     mounted() {
-        axios.get('http://api.nytimes.com/svc/topstories/v2/home.json?api-key=23bd2416129e4997bc0009d118e1e635')
-            .then(response => this.news = response.data.results)
-            .catch(error => console.log(error))
+        this.retrieveNews('');
     }
-});
+})
